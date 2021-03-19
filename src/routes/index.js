cf8e2f1a261fa93a4887599cafcf44bc4d741642
Edgar Medina <is715468@iteso.mx>
2021-03-18 18:40:43 -0600

    Created tone-analyzer endpoint to get data from Tone Analyzer service.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,49 @@
+// Definición de router para crear los endpoints
 const router = require('express').Router();
 
+// Dependencias necesarias para hacer peticiones a Tone Analyzer
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+// Configuración de Tone Analyzer
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2016-05-19',
+  authenticator: new IamAuthenticator({
+    apikey: process.env.APIKEY,
+  }),
+  serviceUrl: process.env.URL,
+  disableSslVerification: true,
+});
+
+// Ruta de autor
 router.route('/autor').get((req, res) => {
     res.send({ alumno : "EFMR", servicio : "Cloud Foundry en IBM Cloud" });
   })
 
+// Ruta para hacer peticiones a servicio Tone Analyzer
+router.route('/tone-analyzer').post((req, res) => {
+
+  // console.log(req.body);
+
+  if(req.body.text == null) return res.status(400).json({"error":"text parameter is required!"});
+
+  const toneParams = {
+    toneInput: { 'text': req.body.text },
+    contentType: 'application/json',
+  };
+  
+  toneAnalyzer.tone(toneParams)
+    .then(toneAnalysis => {
+      // console.log(JSON.stringify(toneAnalysis, null, 2));
+
+      return res.status(200).json({ message : "Ok!", data : JSON.stringify(toneAnalysis, null, 2)})
+    })
+    .catch(err => {
+      // console.log('error:', err);
+
+      return res.status(500).json({error : e});
+    });
+});
+
+// Exportamos el router para ser utilizado en app.js
 module.exports = router;
